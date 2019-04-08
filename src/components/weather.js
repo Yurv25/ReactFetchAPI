0b86d1d7c0386f09b22f18169677895918b48417
Yuri Valverde <yvalverde@freedommobile.ca>
2019-04-08 16:36:59 -0400

    Display api data

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,13 +4,15 @@ class Weather extends React.Component {
 
     state = {
         isLoading : true,
-        city : []
+        city : null
     };
     
     async componentDidMount() {
         const url = "http://api.openweathermap.org/data/2.5/weather?id=524901&appid=122591ecd157814b65c4bb7e244974f0";
         const response = await fetch(url);
+        
         const data = await response.json();
+        console.log(data.main);
         this.setState({ city: data.main, isLoading: false});
     }
 
@@ -19,14 +21,18 @@ class Weather extends React.Component {
             return <div>loading...</div>
         }
 
-        if (!this.state.person){
-            return <div> didn't get a person </div>
+        if (!this.state.city){
+            return <div> didn't get it </div>
         }
         return(
             <div>
-                <div>{this.state.city.main.temp}</div>
+                <div>{this.state.city.temp}</div>
+                <div>{this.state.city.pressure}</div>
+                <div>{this.state.city.humidity}</div>
+                <div>{this.state.city.temp_min}</div>
+                <div>{this.state.city.temp_max}</div>
             </div>
-        )
+        );
     }
 
 }
